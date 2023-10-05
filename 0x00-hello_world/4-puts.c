60a1e9bd4ae34e29899882e2e5b0cdcba101fc72
mmxrtine <michellediawara@gmail.com>
2023-10-05 13:20:37 -0700

    use puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main- aprogram that prints a statement using puts function
+ * Return: 0 (success)
+ */
+int main (void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
