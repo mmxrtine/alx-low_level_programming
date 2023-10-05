43c34d90102a05eea48a56b815ac16c7ad73ec92
mmxrtine <michellediawara@gmail.com>
2023-10-05 13:44:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main- print string using printf
+ * return 0 (succeed)
+ */
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
