c2daf5c1bbb4f525c37f295a08f1fb3d129329b1
mmxrtine <michellediawara@gmail.com>
2023-10-05 14:19:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: this function prints the size of types
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
+
+return (0);
+}
