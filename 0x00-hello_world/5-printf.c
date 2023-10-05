05276860857961d33f9ee43b8ef0a885985f4c8a
mmxrtine <michellediawara@gmail.com>
2023-10-05 13:50:59 -0700

    5-printf.c m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main- print string using printf
+ * main -a program that prints string using printf
  * return 0 (succeed)
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
