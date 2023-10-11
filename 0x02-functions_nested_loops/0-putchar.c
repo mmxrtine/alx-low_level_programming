85825cef99eff211965e3584954118128607230f
mmxrtine <michellediawara@gmail.com>
2023-10-11 12:08:52 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar" followed by a new line.
+ *
+ * Return: 0 to indicate successful execution.
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
