7ff33ac8568fe44dd21c65085b919cbfd281400f
mmxrtine <michellediawara@gmail.com>
2023-10-12 07:48:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function
+ *
+ * return: 0 (success)
+ */
+
+void print_alphabet(void);
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
