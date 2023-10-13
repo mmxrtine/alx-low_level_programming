083349139189279e8ad9cf1b6376bb36e7ae2d67
mmxrtine <michellediawara@gmail.com>
2023-10-13 09:06:00 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,12 @@
  * more_numbers - print more numbers
  */
 
-void more_numbers(void);
+void more_numbers(void)
 
 {
-int i, j;
-for (i = 1; i <= 10; i++)
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
