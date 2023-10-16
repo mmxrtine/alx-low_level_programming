6a61db4a427ba18466d0651236a181599bb667d8
mmxrtine <michellediawara@gmail.com>
2023-10-16 13:47:22 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * _puts- main function 
+ * @str: function parameter
+ */
+
 void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar {str[i]};
-		_putchar ('\n');
+		_putchar (str[i]);
 	}
+
+	_putchar('\n');
 }
