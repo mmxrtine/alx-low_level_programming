fcce39d97ff3975e358873fa6f6b388cfe18c873
mmxrtine <michellediawara@gmail.com>
2023-10-19 10:17:15 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,16 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 
 /* copy string */
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (i < n && src[i] != '\0')
 	{
-		while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
+		dest[i] = src[i];
+		i++;
 	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
