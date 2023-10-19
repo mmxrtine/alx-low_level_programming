fcd4fb9da79b1bf2068da20570d614030de451a2
mmxrtine <michellediawara@gmail.com>
2023-10-19 08:44:18 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - main function 
+ * _strcmp - main function
  * @s1: first parameter
  * @s2: second parameter
  * Return: s1, s2
@@ -9,13 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0
+	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return(s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
