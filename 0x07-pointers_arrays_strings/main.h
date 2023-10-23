6e72585e5b66b791d166eb5e67e7ffec0d82f0e8
mmxrtine <michellediawara@gmail.com>
2023-10-23 11:29:25 -0700

    1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,18 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
-char *rot13(char *a);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8]);
 
 #endif
