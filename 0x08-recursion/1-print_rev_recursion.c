98dd24c62f2905190ec7db32ceb94558ea8946d3
mmxrtine <michellediawara@gmail.com>
2023-10-25 13:17:51 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,11 @@
 #include "main.h"
-/*
- * _print_rev_recursion- main function
- * @s: parameter
- */
+
+/**
+  * _print_rev_recursion - main function
+  *
+  * @s: Function parameter
+  */
+
 
 void _print_rev_recursion(char *s)
 {
