c56f4da534fe3cb9cd5d5004adc7d96ddd75efb6
mmxrtine <michellediawara@gmail.com>
2023-10-25 12:04:28 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * _puts_recursion - main function
+ * @s: first paramter
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(s)
+		_puts_recursion(s+1)
+	}
+	else
+		_putchar('\n);
+}
