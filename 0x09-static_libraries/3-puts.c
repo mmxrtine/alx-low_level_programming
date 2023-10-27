2ab1106b6956df08b92302cc2d0dec4a8bde00e1
mmxrtine <michellediawara@gmail.com>
2023-10-27 07:31:02 -0700

    0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar (str[i]);
+	}
+
+	_putchar('\n');
+}
