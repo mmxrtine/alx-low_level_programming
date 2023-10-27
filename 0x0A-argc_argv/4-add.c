1e9871fece1a5b26173f2d95ac9e3fa5e28a29cc
mmxrtine <michellediawara@gmail.com>
2023-10-27 14:48:15 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,22 +6,21 @@
 /**
  * check_num - checks if a string contains only digits
  * @str: input string
- *
  * Return: 1 if the string contains only digits, 0 otherwise
  */
 int check_num(char *str)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (count < strlen(str))
-    {
-        if (!isdigit(str[count]))
-        {
-            return (0);
-        }
-        count++;
-    }
-    return (1);
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -33,27 +32,27 @@ int check_num(char *str)
  */
 int main(int argc, char *argv[])
 {
-    int count;
-    int str_to_int;
-    int sum = 0;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-    count = 1;
-    while (count < argc)
-    {
-        if (check_num(argv[count]))
-        {
-            str_to_int = atoi(argv[count]);
-            sum += str_to_int;
-        }
-        else
-        {
-            printf("Error\n");
-            return (1);
-        }
-        count++;
-    }
+	count = 1;
 
-    printf("%d\n", sum);
-    return (0);
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+		}
+
+	printf("%d\n", sum);
+	return (0);
 }
-
