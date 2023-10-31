fbf1eb8216209625122b1f5e7a05ac5d36675b3a
mmxrtine <michellediawara@gmail.com>
2023-10-31 03:48:08 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,11 +6,12 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
-
+	
 	if (s1 == NULL)
 		s1 = "";
 
@@ -43,9 +44,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
-
 }
