589be837aa35276d289c2338f8f1ca83fd2a3bb8
mmxrtine <michellediawara@gmail.com>
2023-11-06 07:31:32 -0800

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,19 @@ struct dog
 	char *owner;
 };
 
+/**
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*/
+
+typedef struct dog dog_t;
+
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
