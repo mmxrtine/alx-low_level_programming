cfc12bcff2fcf693df0e4f0273aebfacd3fa320a
mmxrtine <michellediawara@gmail.com>
2023-11-27 12:31:57 -0800

    0

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, _write;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	_write = write(fd, text_content, strlen(text_content));
+	if(_write == -1 || _write != strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
