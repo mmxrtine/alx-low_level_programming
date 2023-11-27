6a829770a1cafe5bde05b6786c86b9c201aacce7
mmxrtine <michellediawara@gmail.com>
2023-11-27 12:33:29 -0800

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 
 int append_text_to_file(const char *filename, char *text_content)
