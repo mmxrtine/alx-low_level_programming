dfa95b0df4f30e16944ba7ad70da137192a5caee
mmxrtine <michellediawara@gmail.com>
2023-11-27 05:46:13 -0800

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        int fd;
+
+        ssize_t _read, _write;
+
+        char *buffer;
+
+        if(!filename)
+        {
+                return (0);
+        }
+
+        /* open file */
+        fd = open(filenam, O_RDONLY);
+        if (fd == -1)
+                return (0);
+
+        /* buffer */
+        buffer = malloc(sizeof(char) * letters);
+        if (buffer == NULL)
+                return (0);
+
+        /* read */
+        _read = read(fd, buffer, letters);
+        if (_read == -1)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
+
+        /* write */
+        _write = write(STDOUT_FILENO, buffer, _read);
+        if (_write == -1)
+        {
+                free(buffer);
+                close(buffer);
+                return (0);
+        }
+        close(fd);
+        return (_read);
+}
